23936466c850152565519141d2ca9086d5a12777
Asar Akhmetbekov <asarakhmetbekov@MacBook-Pro-Asar.local>
2021-08-28 22:08:33 +0600

    feat: almost done

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,35 +1,61 @@
 class TicTacToe {
     constructor() {
-
+        this.playerSymbol = 'x'
+        this.field = [
+            [null, null, null],
+            [null, null, null],
+            [null, null, null]
+        ]
+        this.winner = null
     }
 
     getCurrentPlayerSymbol() {
-
+        return this.playerSymbol
     }
 
     nextTurn(rowIndex, columnIndex) {
+        if (this.field[rowIndex][columnIndex]) {
+            return
+        }
+        else {
+            this.field[rowIndex][columnIndex] = this.playerSymbol
+            this.playerSymbol = this.playerSymbol === 'x' ? 'o' : 'x'
+        }
 
+        if (this.field[0][0] === this.field[0][1] && this.field[0][1] === this.field[0][2]) this.winner = this.field[0][0]
+        if (this.field[1][0] === this.field[1][1] && this.field[1][1] === this.field[1][2]) this.winner = this.field[1][0]
+        if (this.field[2][0] === this.field[2][1] && this.field[2][1] === this.field[2][2]) this.winner = this.field[2][0]
+        if (this.field[0][0] === this.field[1][0] && this.field[1][0] === this.field[2][0]) this.winner = this.field[0][0]
+        if (this.field[0][1] === this.field[1][1] && this.field[1][1] === this.field[2][1]) this.winner = this.field[0][1]
+        if (this.field[0][2] === this.field[1][2] && this.field[1][2] === this.field[2][2]) this.winner = this.field[0][2]
+        if (this.field[0][0] === this.field[1][1] && this.field[1][1] === this.field[2][2]) this.winner = this.field[0][0]
+        if (this.field[2][0] === this.field[1][1] && this.field[1][1] === this.field[0][2]) this.winner = this.field[2][0]
     }
 
     isFinished() {
-
+        return this.noMoreTurns() || this.winner !== null;
     }
 
     getWinner() {
-
+        return this.winner
     }
 
     noMoreTurns() {
-
+        for (let k = 0; k < this.field.length; k++) {
+            for (let l = 0; l < this.field[k].length; l++) {
+              if (this.field[k][l] === null) return false;
+            }
+        } return true;
     }
 
     isDraw() {
-
+        return this.noMoreTurns() && this.getWinner() ? false : true;
     }
 
     getFieldValue(rowIndex, colIndex) {
-
+        return this.field[rowIndex][colIndex]
     }
 }
 
 module.exports = TicTacToe;
+
